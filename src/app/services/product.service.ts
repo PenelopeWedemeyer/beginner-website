5f8a257113c43285c5516f243872914eb5a7fbfd
Penelope Wedemeyer <pennygracew@gmail.com>
2023-11-18 09:16:42 -0800

    Create basic navigation bar. Created service file with CRUD methods.

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  dataSource: string = 'http://localhost:3000/products';
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> 
+  {
+    return this.http.get<Product[]>(this.dataSource);
+  }
+
+  getProductByID(id: number): Observable<Product> 
+  {
+    return this.http.get<Product>(`${this.dataSource}/${id}`);
+  }
+
+  createNewProduct(newProduct: Product): Observable<Product> 
+  {
+    return this.http.post<Product>(this.dataSource, newProduct);
+  }
+
+  editProductByID(id: number, edittedProduct: Product): Observable<Product> 
+  {
+    return this.http.put<Product>(`${this.dataSource}/${id}`, edittedProduct);
+  }
+
+  deleteProductByID(id: number): Observable<any> 
+  {
+    return this.http.delete<any>(`${this.dataSource}/${id}`);
+  }
+}
