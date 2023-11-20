af1cc4969d881fcbe999e15469053f4e398ef271
Penelope Wedemeyer <pennygracew@gmail.com>
2023-11-19 17:43:51 -0800

    created product-detail componenet and made it lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "about", loadChildren: () => import("./components/about/about.module").then(m => m.AboutModule) },
-  { path: "products", loadChildren: () => import("./components/products/products.module").then(m => m.ProductsModule) }
+  { path: "products", loadChildren: () => import("./components/products/products.module").then(m => m.ProductsModule) },
+  { path: "products/details", loadChildren: () => import("./components/product-detail/product-detail.module").then(m => m.ProductDetailModule) },
+  { path: "**", component: HomeComponent }
 ];
 
 @NgModule({
